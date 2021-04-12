d564bbb2dad9950f138452fc5d8ec732fa76d635
jisuS2 <39258299+jisuS2@users.noreply.github.com>
2021-04-12 23:19:03 +0900

    Add files via upload

---
+++ 0412/06Delegate.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshapclass1
+{
+
+    delegate int MyDelegate(int a, int b);
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class Delegate
+    {
+        static void Main(String[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate Callback;
+            Callback = new MyDelegate(Calc.Plus);
+            Console.WriteLine(Callback(3, 4));
+            Callback = new MyDelegate(Calculator.Minus);
+            Console.WriteLine(Callback(7, 5));
+
+        }
+    }
+}
+
---
+++ 0412/12MultiCastDelegate.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshapclass1
+{
+    delegate void MultiCastDelegate();
+    class Schedule
+    {
+        public void Now()
+        {
+            Console.WriteLine("Time : " + DateTime.Now.ToString());
+        }
+        public static void Today()
+        {
+            Console.WriteLine("Date : " + DateTime.Today.ToString());
+        }
+    }
+
+    class MultiCastApp
+    {
+        public static void Main()
+        {
+            Schedule obj = new Schedule();
+            MultiCastDelegate mcd = new MultiCastDelegate(obj.Now);
+            mcd += new MultiCastDelegate(Schedule.Today);
+            mcd();
+        }
+    }
+}
+
