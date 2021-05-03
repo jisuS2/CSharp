313e432a45e64da34405c73ea046e77ef8f8ebb0
jisuS2 <39258299+jisuS2@users.noreply.github.com>
2021-05-03 10:33:29 +0900

    Add files via upload

---
+++ 0503/093GenericMethod.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class GenericMethod
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            double[] d = { 0.1, 0.2, 0.3 };
+            string[] s = { "tiger", "lion", "zebra" };
+            PrintArray<int>(a);
+            PrintArray<double>(d);
+            PrintArray<string>(s);
+        }
+        private static void PrintArray<T>(T[] a)
+        {
+            foreach (var item in a)
+                Console.Write("{0,8}", item);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 0503/098stack_test.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class StackImplementation
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.WriteLine("Push(" + val + ") ");
+                //Console.WriteLine(" top = " + stack.top);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Pop() = " + stack.Pop());
+            }
+        }
+    }
+}
